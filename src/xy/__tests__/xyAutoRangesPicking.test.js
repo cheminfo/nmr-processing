3ac5ec9bb7329e3c6c5094ee055bb6d8d6e453ab
jobo322 <jose.bolanos@correounivalle.edu.co>
2020-06-13 09:58:48 -0500

    fix(testCase): fix test case of xyAutoRangesPicking

--- src/xy/__tests__/xyAutoRangesPicking.test.js
+++ src/xy/__tests__/xyAutoRangesPicking.test.js
@@ -9,11 +9,11 @@ describe('xyAutoRangesPicking', function () {
   it('a triplet and a quadruplet', () => {
     let ranges = xyAutoRangesPicking(tripletQuadruplet, {});
     expect(ranges).toHaveLength(2);
-    expect(ranges[0]).toMatchCloseTo(
+    expect(ranges[1]).toMatchCloseTo(
       {
         from: 1.96744,
         to: 2.03298,
-        integral: 50,
+        integral: 40,
         signal: [
           {
             multiplicity: 'q',
@@ -24,11 +24,11 @@ describe('xyAutoRangesPicking', function () {
       },
       3,
     );
-    expect(ranges[1]).toMatchCloseTo(
+    expect(ranges[0]).toMatchCloseTo(
       {
         from: 0.97588,
         to: 1.02378,
-        integral: 50,
+        integral: 60,
         signal: [
           {
             multiplicity: 't',
