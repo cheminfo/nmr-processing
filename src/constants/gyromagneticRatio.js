ffb7db8a1af04970f2fc583c1f93246b4edb733a
Luc Patiny <luc@patiny.com>
2020-06-24 10:00:13 +0200

    chore: add todo

--- src/constants/gyromagneticRatio.js
+++ src/constants/gyromagneticRatio.js
@@ -1,4 +1,7 @@
-// source: https://en.wikipedia.org/wiki/Gyromagnetic_ratio
+// sources:
+// https://en.wikipedia.org/wiki/Gyromagnetic_ratio
+
+// TODO: can we have a better source and more digits ? @jwist
 
 export const gyromagneticRatio = {
   '1H': 267.52218744e6,
