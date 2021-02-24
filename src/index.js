c8452f697975ab56d2cb960d85803b33fc424659
jobo322 <jose.bolanos@correounivalle.edu.co>
2021-02-24 09:52:01 -0500

    feat: add analyzer for J-Res 2D spectrum

--- src/index.js
+++ src/index.js
@@ -12,3 +12,4 @@ export * from './xyz/xyzAutoPeaksPicking';
 export * from './constants/gyromagneticRatio';
 export * from './constants/impurities';
 export * from './constants/couplingPatterns';
+export * from './xyz/xyzJResAnalizer';
