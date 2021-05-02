04e164b3e8dd4998ced8e1a5e125bd2663d9c803
jobo322 <jose.bolanos@correounivalle.edu.co>
2021-05-02 07:12:02 -0500

    fix: join overlaped ranges

--- src/peaks/util/joinRanges.js
+++ src/peaks/util/joinRanges.js
@@ -2,10 +2,11 @@ export function joinRanges(ranges) {
   ranges.sort((a, b) => a.from - b.from);
   for (let i = 0; i < ranges.length - 1; i++) {
     if (ranges[i].to > ranges[i + 1].from) {
-      ranges[i].to = ranges[i + 1].to;
+      ranges[i].to = Math.max(ranges[i + 1].to, ranges[i].to);
       ranges[i].signal = ranges[i].signal.concat(ranges[i + 1].signal);
       ranges[i].integral += ranges[i + 1].integral;
       ranges.splice(i + 1, 1);
+      i--;
     }
   }
   return ranges;
